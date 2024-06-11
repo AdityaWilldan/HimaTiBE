61e9a980752d0003b31b7da07758693bc0fb9a9a
wildan aditia <penyihirgamingmajo@gmail.com>
2024-06-11 21:10:15 +0700

    fix :  fixed the TeknoPost API, and added botchat DialogFlow

--- resources/views/user/film.blade.php
+++ resources/views/user/film.blade.php
@@ -125,7 +125,7 @@
                                       <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">To Article</button>
                                     </a>
                                     <p class="text-xs font-bold text-gray-900 dark:text-white">
-                                      {{$posting['isoDate']}}
+                                      {{-- {{$posting['isoDate']}} --}}
                                     </p>
     
                                     {{-- <p class="text-sm text-gray-500 dark:text-gray-400">{{$p->tanggal}}</p> --}}
